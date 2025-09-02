685e796ad01f8d523bb4d9e5cc9440962bf637f4
Marçal Maresma <marcalmaresma@gmail.com>
2025-09-02 23:42:00 +0200

    Canvi de text

--- MD5/comprova_md5.py
+++ MD5/comprova_md5.py
@@ -10,8 +10,8 @@ fitxer2 = "fitxer2.txt"
 md5_1 = calcula_md5(fitxer1)
 md5_2 = calcula_md5(fitxer2)
 
-print(f"MD5 de {fitxer1}: {md5_1}")
-print(f"MD5 de {fitxer2}: {md5_2}")
+print(f"MD5 del {fitxer1}: {md5_1}")
+print(f"MD5 del {fitxer2}: {md5_2}")
 
 if md5_1 == md5_2:
     print("Els fitxers són iguals (MD5 coincidents).")
