0a33f1bf08879690daefdfc0b488901f1f2d5111
Marçal Maresma <marcalmaresma@gmail.com>
2025-09-02 23:40:30 +0200

    Eliminar fitxer de prova

--- demo.py
+++
@@ -1,8 +0,0 @@
-import random
-
-print('\nPrograma de prova\n')
-int = random.randint(0, 20)
-print(f"El nombre aleatori inicial és: {int}")
-int = int // 2
-int = int + 5
-print(f"El nombre aleatori final és: {int}")
