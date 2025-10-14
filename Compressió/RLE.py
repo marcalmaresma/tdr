87460bb30ae33dc0dac0ef5067ee6adbf0b3d3bb
Marçal Maresma <marcalmaresma@gmail.com>
2025-10-14 08:37:54 +0200

    Exemple RLE definitiu

--- Compressió/RLE.py
+++ Compressió/RLE.py
@@ -1,57 +1,64 @@
 # Programa senzill de compressió i descompressió RLE sense funcions
+print("\n--- Compressió RLE ---")
+print("1. Comprimir text")
+print("2. Descomprimir text")
 
-while True:
-    print("\n--- Compressió RLE ---")
-    print("1. Comprimir text")
-    print("2. Descomprimir text")
-    print("3. Sortir")
+opcio = input("Escull una opció: ").strip()
 
-    opcio = input("Escull una opció: ")
+if opcio == "1":
+    text = input("Introdueix el text a comprimir: ")
+    if not text:
+        print("Text buit!")
+        exit()
 
-    if opcio == "1":
-        text = input("Introdueix el text a comprimir: ")
+    resultat = ""
+    comptador = 1
+    lletra_anterior = text[0]
 
-        if not text:
-            print("Text buit!")
-            continue
+    # Recorrem cada lletra a partir de la segona
+    for lletra in text[1:]:
+        if lletra == lletra_anterior:
+            comptador += 1
+        else:
+            resultat += str(comptador) + lletra_anterior
+            lletra_anterior = lletra
+            comptador = 1
 
-        resultat = ""
-        comptador = 1
-        lletra_anterior = text[0]
+    # Afegim l’última seqüència
+    resultat += str(comptador) + lletra_anterior
 
-        # Recorrem cada lletra a partir de la segona
-        for lletra in text[1:]:
-            if lletra == lletra_anterior:
-                comptador += 1
-            else:
-                resultat += str(comptador) + lletra_anterior
-                lletra_anterior = lletra
-                comptador = 1
+    print("Text comprimit:", resultat)
+    exit()
+elif opcio == "2":
+    text_comprimit = input("Introdueix el text comprimit: ")
+    if not text_comprimit:
+        print("Text buit!")
+        exit()
 
-        # Afegim l’última seqüència
-        resultat += str(comptador) + lletra_anterior
+    resultat = ""
+    numero = ""
 
-        print("Text comprimit:", resultat)
-
-    elif opcio == "2":
-        text_comprimit = input("Introdueix el text comprimit: ")
-
-        resultat = ""
-        numero = ""
-
-        # Recorrem cada caràcter
-        for caracter in text_comprimit:
-            if caracter.isdigit():
-                numero += caracter
-            else:
+    # Recorrem cada caràcter
+    for caracter in text_comprimit:
+        if caracter.isdigit():
+            numero += caracter
+        else:
+            if not numero:
+                print("Format invàlid: falta nombre abans del caràcter")
+                exit()
+            try:
                 resultat += caracter * int(numero)
-                numero = ""
+            except ValueError:
+                print("Nombre invàlid a la cadena comprimida")
+                exit()
+            numero = ""
 
-        print("Text descomprimit:", resultat)
+    if numero:
+        print("Format invàlid: cadena acaba amb número sols")
+        exit()
 
-    elif opcio == "3":
-        print("Adeu!")
-        break
-
-    else:
-        print("Opció no vàlida, torna-ho a provar.")
+    print("Text descomprimit:", resultat)
+    exit()
+else:
+    print("Opció no vàlida, torna-ho a provar.")
+    exit()
