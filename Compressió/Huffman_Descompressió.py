bea713242d1020058d9f9e5c7a9a18511fa62fbe
Marçal Maresma <marcalmaresma@gmail.com>
2025-10-15 17:39:23 +0200

    Huffman descompressió

---
+++ Compressió/Huffman_Descompressió.py
@@ -0,0 +1,14 @@
+import ast
+
+Arbre = input("Introdueix el diccionari amb l'arbre de Huffman:")
+Arbredef = ast.literal_eval(Arbre)
+print(Arbredef)
+Comprimit = input("Introdueix el text comprimit en binari:")
+Num = ""
+for i in Comprimit:
+    Num += i
+    for k, v in Arbredef.items():
+        if Num == v:
+            print(k, end="")
+            Num = ""
+            break
